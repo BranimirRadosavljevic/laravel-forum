dd41807ef1c864f471c00cddaa391eff7fbae32d
Branimir Radosavljevic <rbranimir@ymail.com>
2020-04-19 13:34:43 +0200

    Episode 11

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,7 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('threads', 'ThreadController@index');
+Route::get('threads/{channel:slug}', 'ThreadController@index');
 Route::get('threads/create', 'ThreadController@create');
 Route::get('threads/{channel}/{thread}', 'ThreadController@show');
 Route::post('threads', 'ThreadController@store');
