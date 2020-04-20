612325f67b5ff1ebb258480d6b07487067e87df4
Branimir Radosavljevic <rbranimir@ymail.com>
2020-04-20 16:15:46 +0200

    Episode 15

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Channel;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -24,6 +25,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        \View::share('channels', Channel::all());
+        View::share('channels', Channel::all());
     }
 }
