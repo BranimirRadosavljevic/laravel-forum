ab0b32b8866c5a8773e26938249a639721142968
Branimir Radosavljevic <rbranimir@ymail.com>
2020-05-03 15:01:27 +0200

    Episode 64

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -8,16 +8,28 @@
                 <h1>
                     {{$profileUser->name}}
                 </h1>
+
+                @can('update', $profileUser)
+                <form method="POST" action="{{ route('avatar', $profileUser) }}" enctype="multipart/form-data">
+                    @csrf
+                    <input type="file" name="avatar">
+
+                    <button type="submit" class="btn btn-primary d-flex mt-2">Add Avatar</button>
+                </form>
+                @endcan
+
+                <img src="{{ asset('/storage/' . $profileUser->avatar_path)}}" width="100" height="100">
             </div>
 
             @forelse ($activities as $date => $activity)
-                <h3 class="pb-1 mt-4 mb-4 border-bottom">{{ $date }}</h3>
+            <h3 class="pb-1 mt-4 mb-4 border-bottom">{{ $date }}</h3>
 
-                @foreach ($activity as $record)
-                    @include("profiles.activities.{$record->type}", ['activity' => $record])
-                @endforeach
+            @foreach ($activity as $record)
+            @include("profiles.activities.{$record->type}", ['activity' => $record])
+            @endforeach
+
             @empty
-                <p>There is no activities for this user yet.</p>
+            <p>There is no activities for this user yet.</p>
             @endforelse
 
             {{-- {{ $threads->links() }} --}}
