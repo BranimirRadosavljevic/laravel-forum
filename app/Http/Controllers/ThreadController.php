388528e9aa6da05570accf2bd0d1d86e9dc9075c
Branimir Radosavljevic <rbranimir@ymail.com>
2020-05-04 14:32:35 +0200

    Episode 66

--- app/Http/Controllers/ThreadController.php
+++ app/Http/Controllers/ThreadController.php
@@ -9,6 +9,7 @@ use App\Thread;
 use App\User;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
 
 class ThreadController extends Controller
 {
@@ -32,7 +33,9 @@ class ThreadController extends Controller
         //     return $threads;
         // }
 
-        return view('threads.index', compact('threads'));
+        $trending = array_map('json_decode', Redis::zrevrange('trending_threads', 0, 4));
+
+        return view('threads.index', compact('threads', 'trending'));
     }
 
     /**
@@ -87,6 +90,12 @@ class ThreadController extends Controller
         if (auth()->check()) {
             auth()->user()->read($thread);
         }
+
+        Redis::zincrby('trending_threads', 1, json_encode([
+            'title' => $thread->title, 
+            'path' => $thread->path()
+        ]));
+
         return view('threads.show', compact('thread'));
     }
 
