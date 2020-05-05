8bec52750723160119dd0049d0ea772708c6dbd3
Branimir Radosavljevic <rbranimir@ymail.com>
2020-05-05 12:50:17 +0200

    Episode 73

--- app/Http/Controllers/Auth/RegisterConfirmationController.php
+++ app/Http/Controllers/Auth/RegisterConfirmationController.php
@@ -10,9 +10,13 @@ class RegisterConfirmationController extends Controller
 {
     public function index()
     {
-        User::where('confirmation_token', request('token'))
+        try{
+            User::where('confirmation_token', request('token'))
             ->firstOrFail()
             ->update(['confirmed' => true]);
+        } catch(\Exception $e) {
+            return redirect('/threads')->with('flash', 'Invalid token.');
+        }
             
         return redirect('/threads')->with('flash', 'Your account is now confirmed! You may post to the forum.');
     }
