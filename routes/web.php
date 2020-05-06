0ede74316e579765328d2aa94db8fd6ba94dd1a3
Branimir Radosavljevic <rbranimir@ymail.com>
2020-05-06 16:10:51 +0200

    Episode 85

--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,9 @@ Route::get('threads/{channel}/{thread}', 'ThreadController@show');
 Route::delete('threads/{channel}/{thread}', 'ThreadController@destroy');
 Route::post('threads', 'ThreadController@store')->middleware('must-be-confirmed');
 Route::get('/threads/{channel}/{thread}/replies', 'ReplyController@index');
+
+Route::post('locked-threads/{thread}', 'LockedThreadsController@store')->name('locked-threads.store')->middleware('admin');
+
 Route::post('/threads/{channel}/{thread}/replies', 'ReplyController@store');
 Route::patch('/replies/{reply}', 'ReplyController@update');
 Route::delete('/replies/{reply}', 'ReplyController@destroy')->name('replies.destroy');
