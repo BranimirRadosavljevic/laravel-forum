59d6313e44708f2753f09181b625c806a04c46a0
Branimir Radosavljevic <rbranimir@ymail.com>
2020-05-15 13:12:30 +0200

    Episode 95

--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -9,6 +9,22 @@
             {{ $threads->render()}}
         </div>
         <div class="col-md-4">
+            <div class="card mb-4">
+                <div class="card-header">
+                    Search
+                </div>
+                <div class="card-body">
+                    <form method="GET" action="/threads/search">
+                        <div class="form-group">
+                            <input type="text" placeholder="Search for something..." name="q" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <button class="btn btn-outline-secondary btn-sm" type="submit">Search</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+
             @if (count($trending))
                 
                 <div class="card">
--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,7 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('threads', 'ThreadController@index');
 Route::get('threads/create', 'ThreadController@create');
+Route::get('threads/search', 'SearchController@show');
 Route::get('threads/{channel:slug}', 'ThreadController@index');
 Route::get('threads/{channel}/{thread}', 'ThreadController@show');
 Route::patch('threads/{channel}/{thread}', 'ThreadController@update');
